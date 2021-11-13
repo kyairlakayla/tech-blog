066106cb11097394870ed8444c58ea2fbc8b6a18
Kayla <kyairlakayla.davis@gmail.com>
2021-11-13 12:10:31 -0500

    issue with login function

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,7 +4,7 @@ const { User, Post } = require('../../models');
 // get all users 
 router.get ('/', (req, res) => {
     User.findAll({
-        attributes: { exclude: ['password'] }
+        attributes: [ 'id', 'username', 'email', 'password' ]
     })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -54,6 +54,31 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.post('/login',  (req, res) => {
+    User.findOne({
+        where: {
+        username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that username!' });
+        return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });  
 });
 
 module.exports = router;
