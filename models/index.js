f9710466ec881a06c3e9479ca3da0928a63c73c7
Kayla <kyairlakayla.davis@gmail.com>
2021-11-14 07:03:55 -0500

    start homepage layout

--- models/index.js
+++ models/index.js
@@ -7,8 +7,16 @@ User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
 Post.belongsTo(User, {
     foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
 });
 
 Comment.belongsTo(User, {
--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(session(sess));
 
 app.use(routes);
 
